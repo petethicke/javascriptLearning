d13a67333492a03b0571a538b7bad93dbe9147cc
Peter Thicke <peterbthicke@gmail.com>
2016-03-14 20:05:31 -0700

    struggling with board layout, trying to set board from user input

--- etchASketch/main.js
+++ etchASketch/main.js
@@ -3,6 +3,8 @@ $(function() {
   $("button").on("click", function() {
     var size = prompt ("Enter a number between 10 and 100.");
     var squares = Math.ceil(parseInt(size))*Math.ceil(parseInt(size));
+    var boardSize = Math.ceil(size);
+    $(".board").css({'height': 'boardSize'+'rem', 'width': 'boardSize'+'rem'});
     var square = "<div class='square'></div>"
     for(var i = 0; i <= squares; i++) {
       $(".board").append(square);
